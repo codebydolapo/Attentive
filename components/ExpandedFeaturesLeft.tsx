09019fac0a54dfee75ffaaead881ffed362aa955
codebydolapo <bashorun115@gmail.com>
2023-01-09 04:15:08 -0800

    aos

--- components/ExpandedFeaturesLeft.tsx
+++ components/ExpandedFeaturesLeft.tsx
@@ -1,10 +1,20 @@
+import Aos from "aos"
+import "aos/dist/aos.css"
+import { useEffect } from "react"
+
+
 function ExpandedFeaturesLeft() {
+
+    useEffect(()=>{
+        Aos.init({duration: 1500})
+    }, [])
+
     return (
         <section className={`w-full md:min-h-[22rem] flex md:flex-row my-[3rem] xs:flex-col-reverse`}>
-            <div className={`md:w-[50%] h-full flex items-center justify-center xs:w-[98%] `}>
+            <div className={`md:w-[50%] h-full flex items-center justify-center xs:w-[98%] `} data-aos = "fade-right">
                 <img className={`md:h-full xs:h-[15rem]`} alt='' src='/images/database.svg' />
             </div>
-            <div className={`md:w-[50%] h-full flex flex-col items-start justify-between p-2 xs:w-[98%] `}>
+            <div className={`md:w-[50%] h-full flex flex-col items-start justify-between p-2 xs:w-[98%] `} data-aos = "fade-left">
                 <h1 className={`md:text-[2rem] font-[1000] text-[#1e95d4] xs:text-lg`}>Semi-Centralized Lecture Database Systems</h1>
                 <p className={`text-black md:text-lg py-2 xs:text-base`}>While the recorded lectures themselves may exist on centralized database systems, each student upon sign up, will be assigned an ERC-721 token that carries their data and details.</p>
                 <p className={`text-black md:text-lg py-2 xs:text-base`}>
--- components/TestimonialItem.tsx
+++ components/TestimonialItem.tsx
@@ -1,8 +1,16 @@
 import styles from '../styles/testimonialitem.module.css';
+import Aos from "aos"
+import "aos/dist/aos.css"
+import { useEffect } from "react"
 
 function TestimonialItem({name, image, comment}: any) {
+
+    useEffect(()=>{
+        Aos.init({duration: 1500})
+    }, [])
+
     return (
-        <div className={`min-w-[20rem] min-h-[20rem] w-[15rem] h-[15rem] rounded-lg mx-2 px-2 ${styles.testimonialitem}`}>
+        <div className={`min-w-[20rem] min-h-[20rem] w-[15rem] h-[15rem] rounded-lg mx-2 px-2 ${styles.testimonialitem}`} data-aos = "flip-right">
             <div className={`w-full h-[45%] flex items-center justify-center`}>
                 <img className={`w-[8.5rem] h-[8.5rem] rounded-full`} alt='' src={image} />
             </div>
